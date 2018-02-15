c077592a3ef7c455d654ca07f62b7bdc2f88301c
masroor-bayati <masroor.bayati@yahoo.com>
2018-02-16 02:01:48 +0330

    edit

--- src/CountMutations.R
+++ src/CountMutations.R
@@ -1,128 +1,97 @@
-library('doParallel')
-library("BSgenome.Hsapiens.UCSC.hg19")
 
-motifs_list <- c()
+
+start_time <- Sys.time()
+cat('loading libraries')
+s <- Sys.time()
+suppressMessages({
+    library(BSgenome.Hsapiens.UCSC.hg19)
+    library(data.table)
+})
+f <- Sys.time()
+cat(paste0(' (',format(f-s),')\n'))        
+#------------------------------------------------------------------------------------
+cat('importing input_table.csv')
+s <- Sys.time()
+catalog <- fread(paste0('result/',input_table_file_name,'.csv'),sep = ',',header = T)
+catalog$chromosome <- paste0('chr',catalog$chromosome)
+catalog$`position+2` <- catalog$position + 2
+catalog$position <- catalog$position - 2
+colnames(catalog) <- c('sample_id','chr','pos-2','ref','alt','pos+2')
+catalog$strand <- '+'
+ref_G_or_A_rows <- which(catalog$ref == 'G' | catalog$ref == 'A')
+catalog$strand[ref_G_or_A_rows] <- '-'
+nuc <- c(1:4)
+names(nuc) <- c('A','C','G','T')
+rev_nuc <- c(1:4)
+names(rev_nuc) <- c('T','G','C','A')
+catalog$alt[ref_G_or_A_rows] <- names(rev_nuc[nuc[catalog$alt[ref_G_or_A_rows]]])
+f <- Sys.time()
+cat(paste0(' (',format(f-s),')\n'))        
+#------------------------------------------------------------------------------------
+cat('making Granges object')
+s <- Sys.time()
+gr <- makeGRangesFromDataFrame(df = catalog,
+                               keep.extra.columns = T,
+                               ignore.strand = F,
+                               seqnames.field = 'chr',
+                               start.field = 'pos-2',
+                               end.field = 'pos+2',
+                               strand.field = 'strand')
+f <- Sys.time()
+cat(paste0(' (',format(f-s),')\n'))        
+#------------------------------------------------------------------------------------
+cat('extracting contexts')
+s <- Sys.time()
+L2_L1_ref_R1_R2 <- as.character(getSeq(Hsapiens,gr))
+f <- Sys.time()
+cat(paste0(' (',format(f-s),')\n'))        
+#------------------------------------------------------------------------------------
+cat('matching motifs')
+s <- Sys.time()
+L2_L1_ref_R1_R2_alt <- paste0(L2_L1_ref_R1_R2,mcols(gr)$alt)
+motifs <- c()            
 for(mut in c('CA','CG','CT','TA','TC','TG')) {
-  for(left1 in c('A','C','G','T')) {
-    for(right1 in c('A','C','G','T')) {
-      for(left2 in c('A','C','G','T')) {
-        for(right2 in c('A','C','G','T')) {
-          ref_c <- paste0(left2,left1,substr(mut,1,1),right1,right2)
-          alt <-substr(mut,2,2)
-          motif <- paste0(ref_c,alt)
-          motif_eq <- paste0(reverseComplement(DNAString(ref_c)),complement(DNAString(alt)))
-          motifs_list <- c(motifs_list,motif,motif_eq)
+    for(left1 in c('A','C','G','T')) {
+        for(right1 in c('A','C','G','T')) {
+            for(left2 in c('A','C','G','T')) {
+                for(right2 in c('A','C','G','T')) {
+                    motifs <- c(motifs,paste0(left2,left1,substr(mut,1,1),right1,right2,substr(mut,2,2))) #L2_L1_ref_R1_R2_alt
+                }
+            }
         }
-      }
     }
-  }
 }
-motif_id <- sort(rep(c(1:(6*16*16)),2))
-names(motif_id) <- motifs_list
+motifs_nums <- match(L2_L1_ref_R1_R2_alt,motifs)
+f <- Sys.time()
+cat(paste0(' (',format(f-s),')\n'))        
+#------------------------------------------------------------------------------------
+cat('making motif matrixes')
+s <- Sys.time()
+sample_ids <- mcols(gr)$sample_id
+sample_names <- unique(sample_ids)
+sample_motif_list <- lapply(sample_names,function(s){
+    cata <- rep(0,16*16*6)
+    tt <- table(motifs_nums[which(sample_ids == s)])
+    cata[as.numeric(names(tt))] <- as.numeric(tt)
+    return(cata)
+})
+f <- Sys.time()
+cat(paste0(' (',format(f-s),')\n'))        
+#------------------------------------------------------------------------------------
+cat('saving the results')
+s <- Sys.time()
+M5mer <- simplify2array(sample_motif_list)
+M3mer <- t(simplify2array(lapply(c(0:95),function(i) colSums(M5mer[c((i*16+1):(i*16+16)),]))))
 
-count_mutations <- function(tt)
-{
-  
-  CHROM <- as.character(tt$chromosome)
-  POS <- as.character(tt$position)
-  REF <- as.character(tt$reference)
-  ALT <- as.character(tt$mutated_to)
-  
-  MT_indxs <- which(CHROM=='MT')
-  if(length(MT_indxs) != 0){CHROM[MT_indxs] <- 'M'}
-  
-  L1.L2.R.R1.R2.A <- paste0(BSgenome::getSeq(Hsapiens,paste0('chr',CHROM),as.numeric(POS)-2,as.numeric(POS)+2),ALT) # 5-mer context
-  
-  motifs_ids <- as.numeric(motif_id[L1.L2.R.R1.R2.A])
-  
-  tm <- table(motifs_ids)
-  counts <- rep(0,(6*16*16))
-  counts[as.numeric(names(tm))] <- as.numeric(tm)
-  
-  return(counts)
-}
+if(save_M5mer == T){fwrite(data.frame(M5mer),file=paste0('result/',M5mer_file_name,'.csv'),sep=",",col.names=FALSE,row.names=FALSE)}
+if(save_M3mer == T){fwrite(data.frame(M3mer),file=paste0('result/',M3mer_file_name,'.csv'),sep=",",col.names=FALSE,row.names=FALSE)}
 
+write.table(sample_names,'result/Samples_IDs.txt',row.names = F,col.names = F)
 
-setProgress(0.2, detail = 'Opening input table...')
-
-catalog <- read.table(paste0('result/',input_table_file_name,'.csv'),sep = ',',header = T)
-
-
-setProgress(0.3, detail = 'Extracting tables of each sample...')
-cat('\nExtracting tables of each sample...')
-sample_names <- unique(catalog$sample_id)
-# ---------------------
-write.table(sample_names,file='result/Samples_IDs.txt',row.names=F,col.names=F)
-# ---------------------
-sample_indxs <- c(1:length(sample_names))  
-separator <- function(i) {catalog[which(catalog$sample_id == sample_names[i]),]}
-
-#---------------------------------------------------------------------------------------------------------------
-if(number_of_cpu_cores > 1)
-{
-  cat('prepareing for parallelization...')
-  st_prepare <- Sys.time()
-  cl <- makeCluster(number_of_cpu_cores)
-  registerDoParallel(cl)
-  clusterCall(cl, function(x) .libPaths(x), .libPaths())
-  se_prepare <- Sys.time()
-  cat(paste0('Done (after ',format(se_prepare-st_prepare),')...'))
-  
-  cat('parallelizing...')
-  sample_tables <- foreach(i=1:length(sample_indxs)) %dopar% {separator(i)}
-  stopCluster(cl)
-} else {
-  sample_tables <- lapply(sample_indxs,separator)
-}
-#---------------------------------------------------------------------------------------------------------------
-cat('All tables extracted.\n')
-
-
-
-# setProgress(0.6, detail = 'Counting mutations...\nIt might take several minutes to finish this step.')
-
-cat('\nCounting mutations...')
-
-#---------------------------------------------------------------------------------------------------------------
-if(number_of_cpu_cores > 1)
-{
-  cat('prepareing for parallelization...')
-  st_prepare <- Sys.time()
-  cl <- makeCluster(number_of_cpu_cores)
-  registerDoParallel(cl)
-  clusterCall(cl, function(x) .libPaths(x), .libPaths())
-  se_prepare <- Sys.time()
-  cat(paste0('Done (after ',format(se_prepare-st_prepare),')...'))
-  
-  cat('parallelizing...')
-  counts <- foreach(i=1:length(sample_tables),.packages = c("BSgenome.Hsapiens.UCSC.hg19")) %dopar% {count_mutations(sample_tables[[i]])}
-  stopCluster(cl)
-} else {
-  counts <- lapply(sample_tables,count_mutations)
-}
-#---------------------------------------------------------------------------------------------------------------
-
-cat('All mutations counted.\n')
-
-
-setProgress(0.9, detail = 'Saving the preprocessing results...')
-
-
-
-
-# For 5-mer:
-M_5mer <- simplify2array(counts)
-#M_5mer <- t(t(M_5mer)/colSums(M_5mer)) # Normalize catalog
-if(save_M5mer == T){write.table(data.frame(M_5mer),file=paste0('result/',M5mer_file_name,'.csv'),sep=",",col.names=FALSE,row.names=FALSE)}
-
-# For 3-mer:
-M_3mer <- t(simplify2array(lapply(c(0:95),function(i) colSums(M_5mer[c((i*16+1):(i*16+16)),]))))
-#M_3mer <- t(t(M_3mer)/colSums(M_3mer)) # Normalize catalog
-if(save_M3mer == T){write.table(data.frame(M_3mer),file=paste0('result/',M3mer_file_name,'.csv'),sep=",",col.names=FALSE,row.names=FALSE)}
-
-
-setProgress(1, detail = 'Preprocessing finished!')
-
-
+f <- Sys.time()
+cat(paste0(' (',format(f-s),')\n'))        
+#------------------------------------------------------------------------------------
+finish_time <- Sys.time()
+cat(paste0('total elapsed time: (',format(finish_time-start_time),')'))
 
 
